886fd5023d110c8d2a57d296c6ca5baab1a79b83
Nik Treiber <treiber@me.com>
2014-01-23 20:04:04 +0100

    A simple test commit with Tower

---
+++ _js-video-player.php
@@ -0,0 +1,15 @@
+<script>
+	// run script after document is ready
+	$(function() {
+	
+		// install flowplayer to an element with CSS class "player"
+		$(".video-player").flowplayer({ 
+			
+			native_fullscreen: true,
+			splash: true,
+			keyboard: false
+			
+		});
+	
+	});
+</script>
